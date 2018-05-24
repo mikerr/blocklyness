a3e14e8861d55b69273fe9814001ddbba3f21970
Mike Redrobe <mike@redrobe.net>
2018-05-24 13:54:59 +0100

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -1,8 +1,9 @@
+//Draw text on canvas
 Blockly.Blocks['show_text'] = {
   init: function() {
-    this.appendDummyInput().appendField('show text: ');
-    this.appendValueInput('VALUE')
-        .setCheck('String')
+    this.appendDummyInput().appendField('Show text ');
+    this.appendValueInput('TEXT')
+        .setCheck('String');
     this.setInputsInline(true);
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
@@ -11,21 +12,36 @@ Blockly.Blocks['show_text'] = {
   }
 };
 Blockly.JavaScript['show_text'] = function(block) {
-  var argument0 = Blockly.JavaScript.valueToCode(block, 'VALUE',
+  var text = Blockly.JavaScript.valueToCode(block, 'TEXT',
       Blockly.JavaScript.ORDER_FUNCTION_CALL) || '\'\'';
-      
-  return 'showtext (' + argument0 + ');\n';
+  var cmd = 'ctx.strokeText(' + text + ',x, y);\n';
+  return cmd;
 };
 
-function showtext (text) {  
-ctx.font = "30px Arial";
-ctx.strokeText(text, x, y);
-}
+// Set the drawing colour
+Blockly.Blocks['set_colour'] = {
+  init: function() {
+    this.appendDummyInput().appendField('Set colour ');
+    this.appendValueInput('VALUE');
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(160);
+    this.setTooltip('Set drawing colour');
+  }
+};
+Blockly.JavaScript['set_colour'] = function(block) {
+  var colour = Blockly.JavaScript.valueToCode(block, 'VALUE',
+      Blockly.JavaScript.ORDER_FUNCTION_CALL) || '\'\'';
+  var cmd ='ctx.strokeStyle = ' + colour + ';\n';
+  return cmd;
+};
 
+//Draw a circle
 Blockly.Blocks['draw_circle'] = {
   init: function() {
-    this.appendDummyInput().appendField('draw_circle: ');
-    this.appendValueInput('VALUE')
+    this.appendDummyInput().appendField('Draw circle ');
+    this.appendValueInput('RADIUS');
     this.setInputsInline(true);
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
@@ -34,23 +50,22 @@ Blockly.Blocks['draw_circle'] = {
   }
 };
 Blockly.JavaScript['draw_circle'] = function(block) {
-  var argument0 = Blockly.JavaScript.valueToCode(block, 'VALUE',
+  var radius = Blockly.JavaScript.valueToCode(block, 'RADIUS',
       Blockly.JavaScript.ORDER_FUNCTION_CALL) || '\'\'';
-      
-  return 'draw_circle (' + argument0 + ');\n';
+  var cmd = '// Draw circle\n'
+  cmd+= 'ctx.beginPath();\n';
+  cmd += 'ctx.arc(x, y, ' + radius + ', 0, 2 * Math.PI);\n';
+  cmd += 'ctx.stroke();\n\n';
+  return cmd;
 };
 
-function draw_circle (size) {  
-ctx.beginPath();
-ctx.arc(x, y, size, 0, 2 * Math.PI);
-ctx.stroke();
-}
 
+//Draw a line
 Blockly.Blocks['draw_line'] = {
   init: function() {
-    this.appendDummyInput().appendField('draw_line: ');
-    this.appendValueInput('VALUE')
-        .setCheck('String')
+    this.appendDummyInput().appendField('Draw line ');
+    this.appendValueInput('VALUE1');
+    this.appendValueInput('VALUE2');
     this.setInputsInline(true);
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
@@ -59,19 +74,16 @@ Blockly.Blocks['draw_line'] = {
   }
 };
 Blockly.JavaScript['draw_line'] = function(block) {
-  var argument0 = Blockly.JavaScript.valueToCode(block, 'VALUE',
+  var argument0 = Blockly.JavaScript.valueToCode(block, 'VALUE1',
       Blockly.JavaScript.ORDER_FUNCTION_CALL) || '\'\'';
-      
-  return 'draw_line (' + argument0 + ');\n';
+  var argument1 = Blockly.JavaScript.valueToCode(block, 'VALUE2',
+      Blockly.JavaScript.ORDER_FUNCTION_CALL) || '\'\'';   
+  var cmd = 'ctx.lineTo (' + argument0 + ',' + argument1 + ');\n';
+  cmd += 'ctx.stroke();\n';
+  return cmd;
 };
 
-
-function draw_line (text) {  
-ctx.moveTo(0, 0);
-ctx.lineTo(200, 100);
-ctx.stroke();
-}
-
+//Clear the Canvas
 Blockly.Blocks['clear_screen'] = {
   init: function() {
     this.appendDummyInput().appendField('Clear Screen');
@@ -82,28 +94,23 @@ Blockly.Blocks['clear_screen'] = {
   }
 };
 Blockly.JavaScript['clear_screen'] = function(block) {
-  var argument0 = Blockly.JavaScript.valueToCode(block, 'VALUE',
-      Blockly.JavaScript.ORDER_FUNCTION_CALL) || '\'\'';
-      
-  return 'clear_screen (' + argument0 + ');\n';
+  var cmd = 'var c = document.getElementById("myCanvas");\n';
+  cmd += 'var ctx = c.getContext("2d");\n';
+  cmd += 'ctx.font = "30px Arial";\n';
+  cmd+='var x = c.width / 2 ;\n';
+  cmd+='var y = c.height / 2 ;\n';
+  cmd += 'ctx.clearRect(0, 0, c.width, c.height);\n\n';
+  return cmd;
 };
+ 
 
-function clear_screen (text) {  
-c = document.getElementById("myCanvas");
-ctx = c.getContext("2d");
 
-if (typeof x === 'undefined') {
-  x = c.width / 2;
-  y = c.height / 2;
-  }
-ctx.clearRect(0, 0, c.width, c.height);
-}
-
+//Move the drawing position cursor
 Blockly.Blocks['move_to'] = {
   init: function() {
-    this.appendDummyInput().appendField('Move to:');
-    this.appendValueInput('VALUEX')
-    this.appendValueInput('VALUEY')
+    this.appendDummyInput().appendField('Move to ');
+    this.appendValueInput('VALUEX');
+    this.appendValueInput('VALUEY');
     this.setInputsInline(true);
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
@@ -116,11 +123,7 @@ Blockly.JavaScript['move_to'] = function(block) {
       Blockly.JavaScript.ORDER_FUNCTION_CALL) || '\'\'';
   var y = Blockly.JavaScript.valueToCode(block, 'VALUEY',
       Blockly.JavaScript.ORDER_FUNCTION_CALL) || '\'\'';  
-  return 'move_to (' + x + ',' + y + ');\n';
+  var cmd = 'ctx.moveTo (' + x + ',' + y + ');\n';
+  return cmd;
 };
 
-function move_to (movex,movey) {
-    x = movex;
-    y = movey;
-    ctx.moveTo(x,y);
-}
